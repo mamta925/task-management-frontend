187f46687cd64f646fa676afef7946a4ce335dfd
mamta925 <mamta925@users.noreply.github.com>
2021-07-24 13:47:03 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.086c94f24235dc2233eeddd4d9524082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319a3b1779f08d6b815dd85c625ac596",
+    "url": "https://mamta925.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "54537e983215676017ae",
+    "url": "https://mamta925.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "b1db9b3d3a6949aa45c6",
+    "url": "https://mamta925.github.io/task-management-frontend/static/js/2.07c411b7.chunk.js"
+  },
+  {
+    "revision": "54537e983215676017ae",
+    "url": "https://mamta925.github.io/task-management-frontend/static/js/main.03945541.chunk.js"
+  },
+  {
+    "revision": "6db3a91f7306e87e5900",
+    "url": "https://mamta925.github.io/task-management-frontend/static/js/runtime~main.110e1773.js"
+  }
+]);
